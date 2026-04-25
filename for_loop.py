5ae780b8b258a8c5b87500089f58ec9e4684d37a
somueltripura <samueltripura68@gmail.com>
2026-04-25 09:59:45 +0600

    initial for loop

--- for_loop.py
+++ for_loop.py
@@ -10,4 +10,27 @@ for x in fruits:
     if x == "banana":
         break
 
+
+#The continue statement
+x = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        continue
+    print("continue:", x)   
+
+for x in range(6):
+    print("range:", x)
+
+#Increment the sequence with 3 (default is 1):
+for x in range(2, 30, 3):
+    print("range with step:", x)
+
+
+#Else in for loop
+for x in range(6):
+    print("else:", x)
+else:
+    print("Finally finished!")
+
+
     
