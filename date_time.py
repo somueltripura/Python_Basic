d389b341976a251600f633a092c8bef5b9cac6fd
somueltripura <samueltripura68@gmail.com>
2026-04-26 10:46:12 +0600

    date_time.py

---
+++ date_time.py
@@ -0,0 +1,10 @@
+#Import the datetime module and display the current date:
+import datetime
+x = datetime.datetime.now()
+print(x)
+
+#Return the year and name of weekday:
+import datetime
+x = datetime.datetime.now()
+print(x.year)
+print(x.strftime("%A"))
