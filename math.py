2f1b845ec20ff96747928bf6947415134cfaad41
somueltripura <samueltripura68@gmail.com>
2026-04-26 10:51:32 +0600

    math.py

---
+++ math.py
@@ -0,0 +1,16 @@
+#build in math functions
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+print(x)
+print(y) 
+
+#Return the absolute value of a number:
+x = abs(-7.25)
+print(x)
+
+#Return the number of times a specified value occurs in a string:
+txt = "Hello, welcome to my world."
+x = txt.count("o")
+print(x)
+
